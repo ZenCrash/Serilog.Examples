81e4d377d4515db7265c15e86a1ee71d068419e3
Allan J <jacobsen_allan@hotmail.com>
2024-09-13 15:44:58 +0200

    initial commit 5

---
+++ Serilog.Examples.LogToConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog.Context;
+
+namespace Serilog.Examples.LogToConsole
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //Initialize serilog using appsetting.json
+      IConfiguration configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+      Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(configuration)
+        .Enrich.FromLogContext()
+        .CreateLogger();
+
+      //Log examples
+      Log.Verbose("Hello Verbose World!");
+      Log.Debug("Hello Debug World!");
+      Log.Information("Hello Information World!");
+      Log.Warning("Hello Warning World!");
+      Log.Error("Hello Error World!");
+      Log.Fatal("Hello Fatal World!");
+
+      using (LogContext.PushProperty("MeaningOfLife", 42)) //contextual information to your log events
+      {
+        Log.Information("What is the meaning of life");
+      }
+
+    }
+  }
+}
