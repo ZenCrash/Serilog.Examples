ea668f8f3f946c09d4dda43e42e0773dc3f37e74
Allan J <jacobsen_allan@hotmail.com>
2024-09-13 10:42:03 +0200

    Add project files.

---
+++ Serilog.Examples.LogToConsole.Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Serilog.Examples.LogToConsole.Example
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
